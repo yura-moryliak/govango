f263de53ecbd528fbc6f01e051cee0d17727059c
ymrlk <moryliak.y@gmail.com>
2025-02-12 16:44:18 +0200

    story(login-page)FE-0004  - format code  - provide login page template, styles and basic logic

--- ui/.storybook/main.ts
+++ ui/.storybook/main.ts
@@ -1,15 +1,15 @@
-import type { StorybookConfig } from "@storybook/angular";
+import type { StorybookConfig } from '@storybook/angular';
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    "@storybook/addon-onboarding",
-    "@storybook/addon-essentials",
-    "@chromatic-com/storybook",
-    "@storybook/addon-interactions",
+    '@storybook/addon-onboarding',
+    '@storybook/addon-essentials',
+    '@chromatic-com/storybook',
+    '@storybook/addon-interactions',
   ],
   framework: {
-    name: "@storybook/angular",
+    name: '@storybook/angular',
     options: {},
   },
 };
--- ui/src/app/modules/auth/login/login.component.ts
+++ ui/src/app/modules/auth/login/login.component.ts
@@ -1,10 +1,44 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { InputText } from 'primeng/inputtext';
+import { IftaLabel } from 'primeng/iftalabel';
+import { Password } from 'primeng/password';
+import { Button } from 'primeng/button';
+import { Divider } from 'primeng/divider';
+
+interface LoginFormGroupInterface {
+  emailOrPhone: FormControl<string | null>;
+  password: FormControl<string | null>;
+}
 
 @Component({
   selector: 'gvg-login',
-  imports: [],
+  imports: [
+    InputText,
+    IftaLabel,
+    Password,
+    Button,
+    Divider,
+    RouterLink,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginComponent {}
+export class LoginComponent {
+  form: FormGroup<LoginFormGroupInterface> = new FormGroup({
+    emailOrPhone: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
+  });
+
+  login(): void {
+    console.log(this.form.value);
+  }
+}
