75a360e31c4e8b320455115723ce896063f391dd
Bohdan Paniv <bohdan.paniv@intelini.com>
2025-02-16 03:01:25 +0200

    added clean architecture for server side

---
+++ server_asp_net/Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿namespace Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string City { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public bool IsCarOwner { get; set; }
+        public bool SSOGoogleId { get; set; }
+        public int LikesNumber { get; set; }
+        public string Avatar { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ server_asp_net/Infrastructure/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users => Set<User>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+        }
+    }
+}
