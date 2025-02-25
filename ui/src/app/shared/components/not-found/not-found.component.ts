df1e4be59e29d408cd1e615594b2d0520f868586
ymrlk <moryliak.y@gmail.com>
2025-02-25 17:36:57 +0200

    story(page-not-found)FE-0011  - add tooltip to settings button and update layout in various components

--- ui/src/app/shared/components/not-found/not-found.component.ts
+++ ui/src/app/shared/components/not-found/not-found.component.ts
@@ -2,11 +2,10 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Button } from 'primeng/button';
 import { RouterLink } from '@angular/router';
 import { TranslatePipe } from '@ngx-translate/core';
-import { AppSettingsPanelButtonComponent } from '../app-settings-panel-button/app-settings-panel-button.component';
 
 @Component({
   selector: 'gvg-not-found',
-  imports: [Button, RouterLink, TranslatePipe, AppSettingsPanelButtonComponent],
+  imports: [Button, RouterLink, TranslatePipe],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
