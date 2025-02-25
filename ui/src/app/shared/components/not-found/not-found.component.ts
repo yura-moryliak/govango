00783d48adb5cc2d7bcb47b4b959ea27293491bf
ymrlk <moryliak.y@gmail.com>
2025-02-25 17:01:51 +0200

    story(page-not-found)FE-0011  - enhance not found page with user-friendly messages and styling

--- ui/src/app/shared/components/not-found/not-found.component.ts
+++ ui/src/app/shared/components/not-found/not-found.component.ts
@@ -1,8 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Button } from 'primeng/button';
+import { RouterLink } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
+import { AppSettingsPanelButtonComponent } from '../app-settings-panel-button/app-settings-panel-button.component';
 
 @Component({
   selector: 'gvg-not-found',
-  imports: [],
+  imports: [Button, RouterLink, TranslatePipe, AppSettingsPanelButtonComponent],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
