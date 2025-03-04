7ec1ec9018fa3e81469de15473e5b58582639a94
ymrlk <moryliak.y@gmail.com>
2025-03-04 19:00:50 +0200

    chore()NestBE-0001-Install-deps-and-setup  - install deps  - update Angular to build for different envs

--- server_nestjs/src/main.ts
+++ server_nestjs/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORT = process.env.PORT || 1111;
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    await app.listen(process.env.PORT ?? 1111);
+    return await app.listen(PORT)
 }
-bootstrap();
+bootstrap().then(() => console.log(`Server running on URL http://localhost:${PORT}`));
