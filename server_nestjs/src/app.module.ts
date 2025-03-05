1d0ad52b863ef93ed9ab3e877f6c12024d8c94f2
ymrlk <moryliak.y@gmail.com>
2025-03-05 14:29:22 +0200

    chore()NestBE-0001-Install-deps-and-setup  - setup config with .env

--- server_nestjs/src/app.module.ts
+++ server_nestjs/src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      cache: true,
+      isGlobal: true
+    })
+  ],
   controllers: [AppController],
   providers: [],
 })
--- server_nestjs/src/main.ts
+++ server_nestjs/src/main.ts
@@ -1,10 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 const PORT = process.env.PORT || 1111;
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    return await app.listen(PORT)
+    const app = await NestFactory.create<NestExpressApplication>(AppModule);
+    return await app.listen(PORT);
 }
 bootstrap().then(() => console.log(`Server running on URL http://localhost:${PORT}`));
