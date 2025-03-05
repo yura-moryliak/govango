41063d8f0544753181cb864c596e903bbd2f9a68
ymrlk <moryliak.y@gmail.com>
2025-03-05 21:00:21 +0200

    story(auth-users-modules-api-routes)NBE-0002  - implement authentication module with routes

--- server_nestjs/src/app.module.ts
+++ server_nestjs/src/app.module.ts
@@ -3,35 +3,49 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { CacheModule } from '@nestjs/cache-manager';
 import { ServeStaticModule } from '@nestjs/serve-static';
+import { RouterModule } from '@nestjs/core';
 import { join } from 'path';
 import { FallbackController } from './fallback.controller';
 import { typeormFactory } from './db.connection';
+import { apiRoutes } from './routes';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+
+const configurationModules = [
+  RouterModule.register(apiRoutes),
+  ServeStaticModule.forRoot({
+    rootPath: join(
+      __dirname,
+      '../../',
+      'dist',
+      'ui',
+      'browser',
+      'index.html',
+    ),
+    serveRoot: '/',
+  }),
+  ConfigModule.forRoot({
+    envFilePath: '.env.development',
+    cache: true,
+    isGlobal: true,
+  }),
+  TypeOrmModule.forRootAsync({
+    useFactory: typeormFactory,
+    inject: [ConfigService],
+  }),
+  CacheModule.register({
+    isGlobal: true,
+  }),
+];
+const commonModules = [
+  AuthModule,
+  UsersModule
+];
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(
-        __dirname,
-        '../../',
-        'dist',
-        'ui',
-        'browser',
-        'index.html',
-      ),
-      serveRoot: '/',
-    }),
-    ConfigModule.forRoot({
-      envFilePath: '.env.development',
-      cache: true,
-      isGlobal: true,
-    }),
-    TypeOrmModule.forRootAsync({
-      useFactory: typeormFactory,
-      inject: [ConfigService],
-    }),
-    CacheModule.register({
-      isGlobal: true,
-    }),
+    ...configurationModules,
+    ...commonModules
   ],
   controllers: [FallbackController],
   providers: [],
