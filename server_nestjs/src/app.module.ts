d17f34e9bba267d652c75d66c04e4edaa799b682
ymrlk <moryliak.y@gmail.com>
2025-03-05 16:22:45 +0200

    chore()NestBE-0001-Install-deps-and-setup  - setup database connection

--- server_nestjs/src/app.module.ts
+++ server_nestjs/src/app.module.ts
@@ -1,13 +1,19 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from "./app.controller";
-import { ConfigModule } from '@nestjs/config';
+import { typeormFactory } from './db.connection';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: '.env',
+      envFilePath: '.env.development',
       cache: true,
       isGlobal: true
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: typeormFactory,
+      inject: [ConfigService]
     })
   ],
   controllers: [AppController],
