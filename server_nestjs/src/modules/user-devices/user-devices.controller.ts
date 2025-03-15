82ea676c6da8be30ad0c01353cb3c0c3a867c80b
ymrlk <moryliak.y@gmail.com>
2025-03-15 20:34:24 +0200

    story/NestFE-0007-devices  - enhance user devices controller with API response decorators for improved documentation

--- server_nestjs/src/modules/user-devices/user-devices.controller.ts
+++ server_nestjs/src/modules/user-devices/user-devices.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Delete, Get, Param, Req, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOkResponse, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { UserDevicesService } from './user-devices.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { UserDevicesModule } from './user-devices.module';
@@ -12,17 +12,21 @@ export class UserDevicesController {
 
   @UseGuards(JwtAuthGuard)
   @Get()
+  @ApiOkResponse({ description: 'Get user devices' })
+  @ApiUnauthorizedResponse({ description: 'Access denied' })
   async getDevices(@Req() req: Request): Promise<UserDevicesModule> {
     return this.userDevicesService.getDevices(req.user as any);
   }
 
   @UseGuards(JwtAuthGuard)
   @Delete(':deviceId')
+  @ApiOkResponse({ description: 'Delete certain device by deviceId' })
+  @ApiUnauthorizedResponse({ description: 'Access denied' })
   async removeDevice(
-    @Req() req,
+    @Req() req: Request,
     @Param('deviceId') deviceId: string,
   ): Promise<boolean> {
-    await this.userDevicesService.removeDevice(req.user, deviceId);
+    await this.userDevicesService.removeDevice(req.user as any, deviceId);
     return true;
   }
 }
