182d8d32850a4cb366c0b6a90ba716c49b62b7c0
ymrlk <moryliak.y@gmail.com>
2025-03-15 21:34:02 +0200

    story/NestFE-0008-swagger-improvements  - add Swagger example for user device responses

--- server_nestjs/src/modules/user-devices/user-devices.controller.ts
+++ server_nestjs/src/modules/user-devices/user-devices.controller.ts
@@ -8,6 +8,7 @@ import {
 import { UserDevicesService } from './user-devices.service';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { UserDevicesModule } from './user-devices.module';
+import { USER_DEVICE_OK_RESPONSE_EXAMPLE } from './user-device.swagger';
 
 @ApiBearerAuth()
 @Controller()
@@ -16,7 +17,7 @@ export class UserDevicesController {
 
   @UseGuards(JwtAuthGuard)
   @Get()
-  @ApiOkResponse({ description: 'Get user devices' })
+  @ApiOkResponse({ description: 'Get user devices', example: USER_DEVICE_OK_RESPONSE_EXAMPLE })
   @ApiUnauthorizedResponse({ description: 'Access denied' })
   async getDevices(@Req() req: Request): Promise<UserDevicesModule> {
     return this.userDevicesService.getDevices(req.user as any);
@@ -24,7 +25,7 @@ export class UserDevicesController {
 
   @UseGuards(JwtAuthGuard)
   @Delete(':deviceId')
-  @ApiOkResponse({ description: 'Delete certain device by deviceId' })
+  @ApiOkResponse({ description: 'Delete certain device by deviceId', example: true })
   @ApiUnauthorizedResponse({ description: 'Access denied' })
   async removeDevice(
     @Req() req: Request,
