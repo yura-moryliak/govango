3b89776246d8f31c79f6bd941bc5db8b7316ec5f
ymrlk <moryliak.y@gmail.com>
2025-04-01 18:28:35 +0300

    story/NFE-0016-client-user-with-crud  - add test dump for users and cars  - add header component

---
+++ ui/src/app/shared/components/header/header.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'gvg-header',
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HeaderComponent {
+
+}
