142504cb778fa6e8a2e7a852f5414d32e22f4a06
ymrlk <moryliak.y@gmail.com>
2025-04-03 20:03:44 +0300

    story/NFE-0016-client-user-with-crud  - remove unused imports

--- ui/src/app/modules/profile/profile.component.ts
+++ ui/src/app/modules/profile/profile.component.ts
@@ -3,28 +3,22 @@ import { HeaderComponent } from '../../shared/components/header/header.component
 import { Store } from '@ngxs/store';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { AuthState } from '../../shared/states/auth/auth.state';
-import { AsyncPipe } from '@angular/common';
 import { Observable } from 'rxjs';
 import { Avatar } from 'primeng/avatar';
 import { Button } from 'primeng/button';
 import { Tab, TabList, TabPanel, TabPanels, Tabs } from 'primeng/tabs';
-import { Card } from 'primeng/card';
-import { Divider } from 'primeng/divider';
 
 @Component({
   selector: 'gvg-profile',
   imports: [
     HeaderComponent,
-    AsyncPipe,
     Avatar,
     Button,
     Tabs,
     TabList,
     Tab,
     TabPanels,
-    TabPanel,
-    Card,
-    Divider,
+    TabPanel
   ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
