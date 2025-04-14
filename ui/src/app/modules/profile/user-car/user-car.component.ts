022ad9ff02bb3c7446ef4053a644c02035b8d876
ymrlk <moryliak.y@gmail.com>
2025-04-14 22:24:09 +0300

    story/NFE-0021-user-car-restructure-and-stuff  - remove unused responsive options from user car component

--- ui/src/app/modules/profile/user-car/user-car.component.ts
+++ ui/src/app/modules/profile/user-car/user-car.component.ts
@@ -70,24 +70,6 @@ export class UserCarComponent implements AfterViewInit {
 
   isUploadCarImagesSidebarOpened: boolean = false;
   isGalleriaOpened: boolean = false;
-  responsiveOptions: any[] = [
-    {
-      breakpoint: '1500px',
-      numVisible: 5,
-    },
-    {
-      breakpoint: '1024px',
-      numVisible: 3,
-    },
-    {
-      breakpoint: '768px',
-      numVisible: 2,
-    },
-    {
-      breakpoint: '560px',
-      numVisible: 1,
-    },
-  ];
 
   ngAfterViewInit(): void {
     this.handleScrollToView();
